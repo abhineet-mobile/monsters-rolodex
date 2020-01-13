8dfc9c9a19555b69d7ffea9dedab28a6af1ab6b8
abhineet-mobile <avi.program@gmail.com>
2020-01-13 20:48:32 +0530

    Updates

---
+++ precache-manifest.3d2b37c3ca31c936a8c63d74e3de845d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb8476009d0ebffea9e6116e554cbc8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "43b80488c80bd1a0c53c",
+    "url": "/monsters-rolodex/static/css/main.74409725.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43b80488c80bd1a0c53c",
+    "url": "/monsters-rolodex/static/js/main.365ab662.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
